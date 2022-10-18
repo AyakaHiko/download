c5354dfd9eada24f92175015478fd8ab49d374f7
Ayaka <m.rusakk@gmail.com>
2022-10-18 21:59:45 +0200

    Добавьте файлы проекта.

---
+++ download/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace download
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.downloadBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addressBox
+            // 
+            this.addressBox.Location = new System.Drawing.Point(61, 12);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(411, 20);
+            this.addressBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Address";
+            // 
+            // downloadBtn
+            // 
+            this.downloadBtn.Location = new System.Drawing.Point(478, 10);
+            this.downloadBtn.Name = "downloadBtn";
+            this.downloadBtn.Size = new System.Drawing.Size(75, 23);
+            this.downloadBtn.TabIndex = 2;
+            this.downloadBtn.Text = "Download";
+            this.downloadBtn.UseVisualStyleBackColor = true;
+            this.downloadBtn.Click += new System.EventHandler(this.downloadBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 56);
+            this.Controls.Add(this.downloadBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addressBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button downloadBtn;
+    }
+}
+
---
+++ download/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace download
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void downloadBtn_Click(object sender, EventArgs e)
+        {
+            _downloadAsync();
+        }
+
+        private Task _writeAsync(byte[] data, string path) => Task.Factory.StartNew(() => _write(data, path));
+        private void _write(byte[] data, string path)
+        {
+            if (data == null)
+                return;
+
+            using (Stream stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        private Task _downloadAsync() => Task.Factory.StartNew(_download);
+        private void _download()
+        {
+
+            try
+            {
+                if (string.IsNullOrEmpty(addressBox.Text))
+                    return;
+                WebClient webClient = new WebClient();
+                var data = webClient.DownloadData(addressBox.Text);
+
+                Func<string> save = () =>
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "All | *.*";
+                    return saveFileDialog.ShowDialog() != DialogResult.OK ? null : saveFileDialog.FileName;
+                };
+                string path= InvokeRequired ? Invoke(save).ToString() : save();
+                
+                _write(data, path);
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+    }
+}
